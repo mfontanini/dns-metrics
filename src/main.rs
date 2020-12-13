11e730849a35f89b886fde4f013319d44d154505
Matias Fontanini <matias.fontanini@gmail.com>
2020-12-13 14:31:40 -0800

    Expose metrics via HTTP endpoint

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,29 @@ use smol::prelude::*;
 use smol::channel;
 use std::sync::Arc;
 use pcap_async::{Config, Handle, PacketStream};
-use prometheus::Registry;
+use prometheus::{
+    Encoder,
+    Registry,
+    TextEncoder,
+};
 use dns_metrics::{
     processing::PacketProcessor,
     tracking::RequestTracker,
     metrics::Metrics,
 };
+
+async fn collect_metrics(request: tide::Request<Registry>) -> tide::Result<String> {
+    let registry = request.state();
+    let mut buffer = Vec::new();
+    let encoder = TextEncoder::new();
+    let metric_familys = registry.gather();
+    for mf in metric_familys {
+        if let Err(e) = encoder.encode(&[mf], &mut buffer) {
+            warn!("Ignoring prometheus encoding error: {:?}", e);
+        }
+    }
+    Ok(String::from_utf8(buffer.clone()).unwrap())
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     SimpleLogger::new()
@@ -29,6 +46,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let processor = PacketProcessor::new(receiver, RequestTracker::default(), metrics);
     smol::spawn(async move {
         processor.run().await
+    }).detach();
+
+    let mut app = tide::with_state(registry);
+    app.at("/metrics").get(collect_metrics);
+    smol::spawn(async move {
+        app.listen("127.0.0.1:8080").await
     }).detach();
 
     smol::block_on(async move {
